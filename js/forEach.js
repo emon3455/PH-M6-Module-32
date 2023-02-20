4ab3085eda7860bdb1685bd6419912f2980f96fb
emon3455 <emon.mhk69@gmail.com>
2023-02-21 01:43:54 +0600

    filter done

---
+++ js/forEach.js
@@ -0,0 +1,13 @@
+const products = [
+    {id:1, name: "iphone", price: 16000},
+    {id:2, name: "pc", price: 38000},
+    {id:3, name: "laptop", price: 40000},
+];
+
+const arr=[10,20,30,40];
+
+products.forEach(p => console.log(p.name));
+arr.forEach(x => console.log(x));
+
+// if we do use syntax like map function it will not return any think it will provide undefine.
+
